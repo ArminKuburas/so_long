de6baab913befab84a315f1a0d6e05e113d84225
Armin Kuburas <akuburass@gmail.com>
2024-01-22 07:17:52 +0200

    Contuining to learn how MLX works. Just a few simple tests. Will continue to do so this week in hopes to get a general idea on how to do so long@

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # so_long
 This repository will hold all my work for the school 42 project so_long
+test.c MLX/libmlx42.a -Iinclude -lglfw -L"/Users/akuburas/.brew/opt/glfw/lib/"
--- testing_MLX/first_test.c
+++ testing_MLX/first_test.c
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 15:08:52 by akuburas          #+#    #+#             */
-/*   Updated: 2024/01/15 16:51:18 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/01/15 17:29:23 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,44 @@
 #include <stdbool.h>
 #include "../MLX42/MLX42.h"
 
+void my_keyhook(mlx_key_data_t keydata, void *param)
+{
+	// If we PRESS the 'J' key, print "Hello".
+	if (keydata.key == MLX_KEY_J && keydata.action == MLX_PRESS)
+		puts("Hello ");
+
+	// If we RELEASE the 'K' key, print "World".
+	if (keydata.key == MLX_KEY_K && keydata.action == MLX_RELEASE)
+		puts("World");
+
+	// If we HOLD the 'L' key, print "!".
+	if (keydata.key == MLX_KEY_L && keydata.action == MLX_REPEAT)
+		puts("!");
+}
+
+void	my_scrollhook(double xdelta, double ydelta, void *param)
+{
+	// Simple up or down detection.
+	if (ydelta > 0)
+		puts("Up!");
+	else if (ydelta < 0)
+		puts("Down!");
+	
+	// Can also detect a mousewheel that goes along the X (e.g: MX Master 3)
+	if (xdelta < 0)
+		puts("Sliiiide to the left!");
+	else if (xdelta > 0)
+		puts("Sliiiide to the right!");
+}
+
+
 int	main(void)
 {
 	mlx_t	*window;
 
 	window = mlx_init(512, 512, "Hello World", 1);
+	mlx_key_hook(window, my_keyhook, NULL);
 	mlx_loop(window);
+	mlx_terminate(window);
 	return (0);
 }
