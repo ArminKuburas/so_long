8338b8ce321641cdaa49e6952d93287b1876c5a4
Armin Kuburas <akuburass@gmail.com>
2024-01-22 16:19:03 +0200

    Adding some new tests. Getting an idea on how to handle this@

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # so_long
 This repository will hold all my work for the school 42 project so_long
 test.c MLX/libmlx42.a -Iinclude -lglfw -L"/Users/akuburas/.brew/opt/glfw/lib/"
+100 by 100 resolution. Piskel
--- testing_MLX/first_test.c
+++ testing_MLX/first_test.c
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 15:08:52 by akuburas          #+#    #+#             */
-/*   Updated: 2024/01/15 17:29:23 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/01/22 15:04:59 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,26 @@
 #include <stdbool.h>
 #include "../MLX42/MLX42.h"
 
-void my_keyhook(mlx_key_data_t keydata, void *param)
+void	my_keyhook(mlx_key_data_t keydata, mlx_image_t *img)
 {
-	// If we PRESS the 'J' key, print "Hello".
 	if (keydata.key == MLX_KEY_J && keydata.action == MLX_PRESS)
+	{
 		puts("Hello ");
-
-	// If we RELEASE the 'K' key, print "World".
+		img->width += 10;
+	}
 	if (keydata.key == MLX_KEY_K && keydata.action == MLX_RELEASE)
 		puts("World");
-
-	// If we HOLD the 'L' key, print "!".
 	if (keydata.key == MLX_KEY_L && keydata.action == MLX_REPEAT)
 		puts("!");
 }
 
 void	my_scrollhook(double xdelta, double ydelta, void *param)
 {
-	// Simple up or down detection.
 	if (ydelta > 0)
 		puts("Up!");
 	else if (ydelta < 0)
 		puts("Down!");
-	
-	// Can also detect a mousewheel that goes along the X (e.g: MX Master 3)
+
 	if (xdelta < 0)
 		puts("Sliiiide to the left!");
 	else if (xdelta > 0)
@@ -48,10 +44,19 @@ void	my_scrollhook(double xdelta, double ydelta, void *param)
 
 int	main(void)
 {
-	mlx_t	*window;
+	mlx_t			*window;
+	mlx_texture_t	*texture;
+	double			initial_time;
+	mlx_image_t		*img;
 
+	initial_time = mlx_get_time();
 	window = mlx_init(512, 512, "Hello World", 1);
-	mlx_key_hook(window, my_keyhook, NULL);
+	mlx_scroll_hook(window, my_scrollhook, NULL);
+	texture = mlx_load_png("./warrior.png");
+	img = mlx_texture_to_image(window, texture);
+	mlx_key_hook(window, my_keyhook, img);
+	mlx_image_to_window(window, img, 0, 0);
+	mlx_loop_hook(window);
 	mlx_loop(window);
 	mlx_terminate(window);
 	return (0);
