e4e17002b6b53f9e0c1a8b0df3b515899acecacb
Armin Kuburas <akuburass@gmail.com>
2024-01-29 10:04:20 +0200

    A few times it crashes we need to figure out why@

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ This repository will hold all my work for the school 42 project so_long
 test.c MLX/libmlx42.a -Iinclude -lglfw -L"/Users/akuburas/.brew/opt/glfw/lib/"
 100 by 100 resolution. Piskel
 Jonathan Bagger
+cc first_test.c libft.a ../MLX42/libmlx42.a -Iinclude -lglfw -L"/Users/akuburas/.brew/opt/glfw/lib/"
--- testing_MLX/first_test.c
+++ testing_MLX/first_test.c
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 15:08:52 by akuburas          #+#    #+#             */
-/*   Updated: 2024/01/25 17:52:05 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/01/26 17:05:08 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,22 @@ void	my_keyhook(mlx_key_data_t keydata, void *param)
 	t_gdata	*data;
 	int		i;
 
+	data->x = 1;
 	i = 0;
 	data = param;
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
 		mlx_close_window(data->window);
 	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS)
-		while (i < 2)
+		while (i < 6)
 			data->hero_images[i++]->instances[0].y += 15;
 	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
-		while (i < 2)
+		while (i < 6)
 			data->hero_images[i++]->instances[0].y -= 15;
 	if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS)
-		while (i < 2)
+		while (i < 6)
 			data->hero_images[i++]->instances[0].x += 15;
 	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS)
-		while (i < 2)
+		while (i < 6)
 			data->hero_images[i++]->instances[0].x -= 15;
 	if (keydata.key == MLX_KEY_T && keydata.action == MLX_PRESS)
 	{
@@ -41,6 +42,7 @@ void	my_keyhook(mlx_key_data_t keydata, void *param)
 	}
 	if (keydata.key == MLX_KEY_N && keydata.action == MLX_PRESS)
 		mlx_image_to_window(data->window, data->hero_images[0], 0, 0);
+	data->x = 0;
 }
 
 void	my_scrollhook(double xdelta, double ydelta, void *param)
@@ -80,20 +82,33 @@ char	*png_path(char *name, int number, t_gdata *data)
 	return (str);
 }
 
+int	check_button(t_gdata *data)
+{
+	if (mlx_is_key_down(data->window, MLX_KEY_W))
+		return (1);
+	if (mlx_is_key_down(data->window, MLX_KEY_S))
+		return (1);
+	if (mlx_is_key_down(data->window, MLX_KEY_D))
+		return (1);
+	if (mlx_is_key_down(data->window, MLX_KEY_A))
+		return (1);
+	return (0);
+}
+
 void	my_hook(void *param)
 {
 	t_gdata	*data;
 	double	time_since;
 	int		num_frames;
 
-	num_frames = 2;
+	// if (data->x == 1)
+	// 	return ;
+	num_frames = 6;
 	data = param;
 	time_since = mlx_get_time();
-	if (time_since - data->time >= 1)
+	if (time_since - data->time >= 0.05)
 	{
 		data->hero_images[data->hero_frames]->instances->enabled = false;
-		printf("this is time: %f\n", data->time);
-		printf("this is time since: %f\n", time_since);
 		data->hero_frames = (data->hero_frames + 1) % num_frames;
 		data->hero_images[data->hero_frames]->instances->enabled = true;
 		data->time = mlx_get_time();
@@ -101,54 +116,60 @@ void	my_hook(void *param)
 	data->x = 1;
 }
 
-void	my_hook2(void *param)
-{
-	t_gdata		*data;
-	static int	amount;
-	double		time_since;
+// void	my_hook2(void *param)
+// {
+// 	t_gdata		*data;
+// 	static int	amount;
+// 	double		time_since;
 
-	data = param;
-	amount++;
-	time_since = mlx_get_time();
-	if (time_since - data->time >= 1)
-	{
-		data->x = 1;
-		printf("This is the amount of frames per second: %d\n", amount);
-		data->time = mlx_get_time();
-		amount = 0;
-	}
-}
+// 	data = param;
+// 	amount++;
+// 	time_since = mlx_get_time();
+// 	if (time_since - data->time >= 1)
+// 	{
+// 		data->x = 1;
+// 		printf("This is the amount of frames per second: %d\n", amount);
+// 		data->time = mlx_get_time();
+// 		amount = 0;
+// 	}
+// }
 
 int	main(void)
 {
-	t_gdata		*data;
+	t_gdata		data;
 	int			i;
-	static int	x = 0;
 	char		*png;
 
-	data->hero_frames = 0;
-	data->x = 0;
-	data->time = mlx_get_time();
-	// printf("Value at address stored in x: %lf\n", data->time);
-	data->window = mlx_init(512, 512, "Hello World", 1);
-	// mlx_scroll_hook(data->window, my_scrollhook, NULL);
-	// while (i < 2)
-	// {
-	// 	png = (png_path("./warrior_", i, data));
-	// 	printf("this is the png path: %s\n", png);
-	// 	data->hero_textures[i] = mlx_load_png(png);
-	// 	data->hero_images[i] = mlx_texture_to_image(data->window, data->hero_textures[i]);
-	// 	if (mlx_image_to_window(data->window, data->hero_images[i], 50, 50) < 0)
-	// 		;
-	// 	data->hero_images[i]->instances->enabled = false;
-	// 	free(png);
-	// 	i++;
-	// }
-	// data->hero_images[0]->instances->enabled = true;
-	// mlx_key_hook(data->window, my_keyhook, data);
-	// mlx_loop_hook(data->window, my_hook, data);
-	mlx_loop_hook(data->window, my_hook2, data);
-	mlx_loop(data->window);
-	mlx_terminate(data->window);
+	data.hero_frames = 0;
+	data.x = 0;
+	data.time = mlx_get_time();
+	data.window = mlx_init(512, 512, "Hello World", 1);
+	mlx_scroll_hook(data.window, my_scrollhook, NULL);
+	i = 0;
+	while (i < 6)
+	{
+		png = (png_path("./warrior_idle_frames/warrior_", i, &data));
+		printf("this is the png path: %s\n", png);
+		data.hero_textures[i] = mlx_load_png(png);
+		data.hero_images[i] = mlx_texture_to_image(data.window, data.hero_textures[i]);
+		if (mlx_image_to_window(data.window, data.hero_images[i], 50, 50) < 0)
+			;
+		data.hero_images[i]->instances->enabled = false;
+		free(png);
+		// png = (png_path("./warrior_move_frames/warrior_m_", i, &data));
+		// data.hero_m_textures[i] = mlx_load_png(png);
+		// data.hero_m_images[i] = mlx_texture_to_image(data.window, data.hero_m_textures[i]);
+		// if (mlx_image_to_window(data.window, data.hero_m_images[i], 50, 50) < 0)
+		// 	;
+		// data.hero_m_images[i]->instances->enabled = false;
+		// free(png);
+		i++;
+	}
+	data.hero_images[0]->instances->enabled = true;
+	mlx_key_hook(data.window, my_keyhook, &data);
+	mlx_loop_hook(data.window, my_hook, &data);
+	// mlx_loop_hook(data.window, my_hook2, &data);
+	mlx_loop(data.window);
+	mlx_terminate(data.window);
 	return (0);
 }
